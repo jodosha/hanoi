4ea1d91beef5ca1bff5eeda1eb4e4fe1a327437a
Luca Guidi <guidi.luca@gmail.com>
2009-06-09 11:48:03 +0200

    Make it run

--- lib/hanoi/javascript_test_task.rb
+++ lib/hanoi/javascript_test_task.rb
@@ -48,8 +48,7 @@ class JavaScriptTestTask < ::Rake::TaskLib
         end
       end
 
-      # TODO re-enable
-      # @test_builder.teardown
+      destroy_temp_directory
       @server.shutdown
       t.join
     end
@@ -61,8 +60,20 @@ class JavaScriptTestTask < ::Rake::TaskLib
     "http://localhost:4711#{test[:url]}?#{params}"
   end
 
-  def mount(path, dir=nil)
-    dir = Dir.pwd + path unless dir
+  def setup(current_directory, test_cases)
+    @current_directory = current_directory
+    test_cases = prepare_tests(test_cases)
+    run_test_cases(test_cases)
+    mount_paths
+  end
+
+  def mount_paths
+    mount "/",     assets_directory
+    mount "/test", temp_directory
+  end
+
+  def mount(path, dir = nil)
+    dir = @current_directory + path unless dir
 
     # don't cache anything in our tests
     @server.mount(path, NonCachingFileHandler, dir)
@@ -94,4 +105,70 @@ class JavaScriptTestTask < ::Rake::TaskLib
 
     @browsers << browser
   end
+
+  private
+    def prepare_tests(test_cases)
+      create_temp_directory
+      test_cases ||= Dir["#{test_directory}/**/*_test.js"] + Dir["#{test_directory}/**/*_spec.js"]
+      test_cases.map do |test_case|
+        test_case = TestCase.new(test_case)
+        unless test_case.exist?
+          destroy_temp_directory
+          raise "Test case not found: '#{test_case.path}'"
+        end
+        write_template test_case
+        test_case
+      end
+    end
+
+    def run_test_cases(test_cases)
+      test_cases.each { |test_case| run test_case.url }
+    end
+
+    def test_directory
+      @test_directory ||= begin
+        directory = File.directory?(File.expand_path(@current_directory + "/test")) ? "test" : "spec"
+        directory << "/javascript"
+        unless File.directory?(directory)
+raise <<-END
+Can't find JavaScript test directory in '#{@current_directory}'.
+Please make sure at least one of them exist:
+\t'#{@current_directory}/test/javascript'
+\t'#{@current_directory}/spec/javascript'\n
+END
+        end
+        directory
+      end
+    end
+
+    def template
+      @template ||= begin
+        path = File.expand_path(test_directory + "/templates/test_case.erb")
+        raise "Can't find the Javascript test template: '#{path}'" unless File.exist?(path)
+        ERB.new(File.new(path).read)
+      end
+    end
+
+    def write_template(test_case)
+      # instance var is needed by ERb binding.
+      @test_case = test_case
+      template_path = "#{temp_directory}/#{test_case.name}.html"
+      File.open(template_path, 'w') { |f| f.write(template.result(binding)) }
+    end
+
+    def create_temp_directory
+      FileUtils.mkdir_p temp_directory
+    end
+
+    def destroy_temp_directory
+      FileUtils.rm_rf(temp_directory) rescue nil
+    end
+
+    def temp_directory
+      @temp_directory ||= test_directory + "/tmp"
+    end
+
+    def assets_directory
+      @assets_directory ||= File.expand_path(@current_directory + "/test/javascript/assets").freeze
+    end
 end
---
+++ lib/hanoi/test_case.rb
@@ -0,0 +1,34 @@
+class TestCase
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+  
+  def name
+    @name ||= File.basename(@path, '.js')
+  end
+
+  def target
+    # TODO make this configurable
+    "/javascripts/#{name.gsub('_test', '')}.js"
+  end
+
+  def content
+    File.new(@path).read
+  end
+
+  def exist?
+    File.exist?(path)
+  end
+
+  def url
+    "/test/#{name}.html"
+  end
+
+  # TODO enable
+  # def html_fixtures
+  #   path = "#{@plugin.root}/test/javascript/fixtures/#{type}/#{relative_path.gsub("_test.js", "_fixtures.html")}"
+  #   File.new(path).read rescue ""
+  # end
+end
